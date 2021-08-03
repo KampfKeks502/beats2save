000afc0f5aca3d580a3baf052ee5b8afde68e9e2
KampfKeks502 <72569299+KampfKeks502@users.noreply.github.com>
2021-08-03 15:20:50 +0200

    small fix

--- _info.bat
+++ _info.bat
@@ -1,6 +1,6 @@
 @echo off
 title Beats2Save - info
-python Beats2Save.py -g "G:/Steam/steamapps/common" -s "C:/Users/KampfKeks/AppData/LocalLow/Hyperbolic Magnetism" -b "G:\Beat Saber backups" -h
+python Beats2Save.py -g "G:/Steam/steamapps/common" -s "C:/Users/KampfKeks/AppData/LocalLow/Hyperbolic Magnetism" -b "G:\Beat Saber backups" -i
 pause
 
 rem show informations about Beat Saber (BS-Version, Size, Backup dir size)
